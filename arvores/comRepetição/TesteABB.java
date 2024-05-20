a540c66c2a4c9d464389bdddac722b80def9cef2
IRIS DE CAMPOS MELERO <23.01109-2@maua.br>
2024-05-20 11:12:44 -0300

    Aula 14  e 15 - Busca binária em árvores

--- arvores/comRepetição/TesteABB.java
+++ arvores/comRepetição/TesteABB.java
@@ -4,16 +4,16 @@ public class TesteABB {
     public static void main(String[] args) {
         Random random = new Random();
         int n = random.nextInt(501) + 5;
+        System.out.println("\nn = " + n);
         int[] v = new int[n];
         ABB abb = new ABB();
-        for (int i = 0; i<200; i++) {
+        for (int i=0; i<n; i++) {
             v[i] = random.nextInt(n/5) + 1;
             System.out.print(v[i] + " ");
             abb.insere(v[i]);
         }
-        System.out.println("\nn: " + n);
-        System.out.println("\nA árvore:\n" + abb.stringEmOrdem());
-        System.out.println("\nNível: " + abb.nivel());
-        System.out.println("\nNúmero de nós: " + abb.numeroNos() + "\n");
+        System.out.println("\na arvore:\n" + abb.stringEmOrdem());
+        System.out.println("\nnivel: " + abb.nivel());
+        System.out.println("\nnumero de nos: " + abb.numeroNos());
     }
 }
